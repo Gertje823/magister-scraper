3b8802f886a87850473be4785ac0b8920dfc871c
Gertje823 <36937387+Gertje823@users.noreply.github.com>
2023-01-31 18:22:35 +0100

    Added login with credentials
    Added login with credentials instead of token as requested in #2

--- magister.py
+++ magister.py
@@ -1,8 +1,40 @@
 import requests, json
+import asyncio
+from pyppeteer import launch
 
-base_url = 'NAME_HERE.magister.net'
+base_url = 'BASE_URL.magister.net'
+username = 'YOUR_USERNAME'
+password = 'YOUR_PASSWORD'
 
-headers = {'Authorization':'Bearer TOKEN_HERE'}
+# Magister login Pyppeteer
+async def interceptResponse(request):
+    if request.url == f"https://{base_url}/api/m6/applicatietoegang":
+        global headers
+        headers = request.headers
+async def main():
+    browser = await launch({"headless": True, "args": ["--start-maximized"]})
+    page = await browser.newPage()
+    #await page.setRequestInterception(True)
+
+    await page.goto(f"https://{base_url}")
+    # locate the search box
+    entry_box = await page.waitForXPath("""//*[@id="username"]""")
+
+    await entry_box.type(username)
+    await asyncio.sleep(1)
+    await page.click('button')
+    entry_box = await page.waitForXPath("""//*[@id="password"]""")
+    await entry_box.type(password)
+    await asyncio.sleep(1)
+
+    await page.click('#password_submit')
+    page.on('request',
+            lambda request: asyncio.ensure_future(interceptResponse(request)))
+    await asyncio.sleep(1)
+    await browser.close()
+print("Logging in to Magister...")
+asyncio.get_event_loop().run_until_complete(main())
+
 
 r = requests.get(f"https://{base_url}/api/sessions/current", headers=headers)
 account_href = r.json()['links']['account']['href']
